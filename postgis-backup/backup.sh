f242e67d0350ac3f086b975634d2987068b52442
Ali Kaafarani <ali@kvikshaug.no>
2016-11-08 09:23:52 +0100

    Add simple log output

--- postgis-backup/backup.sh
+++ postgis-backup/backup.sh
@@ -6,6 +6,7 @@ source /secrets/postgis.env
 # Specify databases to back up here
 for DATABASE in "sherpa"
 do
+    echo "$(date): Backing up database ${DATABASE}..."
     FILENAME=${DATABASE}-$(date "+%Y%m%d").xz
     pg_dump -h postgres -Fc ${DATABASE} | xz --compress > ${FILENAME}
     aws s3 mv ${FILENAME} s3://turistforeningen/backups/postgis/
